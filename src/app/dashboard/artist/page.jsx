f400b6fc8acadfd5b697dd9d84fab03ae6a01581
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-06-29 03:02:46 +0600

    artist dashboard completed with dynamic data

--- src/app/dashboard/artist/page.jsx
+++ src/app/dashboard/artist/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import { authClient } from "@/lib/auth-client";
+import React, { useEffect, useState } from "react";
 import {
     FaPalette,
     FaDollarSign,
@@ -8,9 +9,39 @@ import {
     FaImage,
 } from "react-icons/fa";
 
+
 const ArtistDashboard = () => {
+
+
+    const { data: session } = authClient.useSession();
+    const email = session?.user?.email;
+
+    const [dashboard, setDashboard] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (!email) return;
+
+        const fetchDashboard = async () => {
+            try {
+                const res = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/artist-dashboard/${email}`
+                );
+
+                const data = await res.json();
+
+                setDashboard(data);
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDashboard();
+    }, [email]);
     return (
-        <div className="p-6 w-[90%] mx-auto">
+        <div className="w-[95%] mx-auto">
 
             <div className="mb-8">
                 <h1 className="text-3xl font-bold text-slate-800">
@@ -31,7 +62,7 @@ const ArtistDashboard = () => {
                                 Total Artworks
                             </p>
                             <h2 className="text-3xl font-bold mt-2">
-                                12
+                                {dashboard?.totalArtworks || 0}
                             </h2>
                         </div>
 
@@ -48,7 +79,7 @@ const ArtistDashboard = () => {
                                 Total Sales
                             </p>
                             <h2 className="text-3xl font-bold mt-2">
-                                24
+                                {dashboard?.totalSales || 0}
                             </h2>
                         </div>
 
@@ -65,8 +96,7 @@ const ArtistDashboard = () => {
                                 Revenue
                             </p>
                             <h2 className="text-3xl font-bold mt-2">
-                                $1,250
-                            </h2>
+                                ${dashboard?.revenue || 0}                            </h2>
                         </div>
 
                         <div className="w-14 h-14 rounded-full bg-green-100 flex items-center justify-center">
@@ -82,7 +112,7 @@ const ArtistDashboard = () => {
                                 Active Listings
                             </p>
                             <h2 className="text-3xl font-bold mt-2">
-                                10
+                                {dashboard?.activeListings || 0}
                             </h2>
                         </div>
 
@@ -107,45 +137,30 @@ const ArtistDashboard = () => {
                         <thead>
                             <tr className="border-b">
                                 <th className="text-left py-3">Title</th>
-                                <th className="text-left py-3">Category</th>
-                                <th className="text-left py-3">Price</th>
-                                <th className="text-left py-3">Status</th>
+                                <th className="text-left py-3 pl-2">Category</th>
+                                <th className="text-left py-3 pl-2">Price</th>
+                                <th className="text-left py-3 pl-2">Status</th>
                             </tr>
                         </thead>
 
                         <tbody>
-                            <tr className="border-b">
-                                <td className="py-3">Sunset Painting</td>
-                                <td>Painting</td>
-                                <td>$120</td>
-                                <td>
-                                    <span className="px-3 py-1 rounded-full bg-green-100 text-green-700 text-sm">
-                                        Active
-                                    </span>
-                                </td>
-                            </tr>
-
-                            <tr className="border-b">
-                                <td className="py-3">Digital Nature</td>
-                                <td>Digital Art</td>
-                                <td>$90</td>
-                                <td>
-                                    <span className="px-3 py-1 rounded-full bg-green-100 text-green-700 text-sm">
-                                        Active
-                                    </span>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td className="py-3">Abstract Colors</td>
-                                <td>Abstract</td>
-                                <td>$150</td>
-                                <td>
-                                    <span className="px-3 py-1 rounded-full bg-yellow-100 text-yellow-700 text-sm">
-                                        Pending
-                                    </span>
-                                </td>
-                            </tr>
+                            {dashboard?.recentArtworks?.map((art) => (
+                                <tr key={art._id} className="border-b">
+                                    <td className="py-3">{art.title}</td>
+                                    <td className="pl-2">{art.category}</td>
+                                    <td className="pl-2">${art.price}</td>
+                                    <td>
+                                        <span
+                                            className={`px-3 pl-2 py-1 rounded-full text-sm ${art.availability === "Sold"
+                                                ? "bg-red-100 text-red-600"
+                                                : "bg-green-100 text-green-600"
+                                                }`}
+                                        >
+                                            {art.availability}
+                                        </span>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
 
                     </table>
@@ -166,26 +181,34 @@ const ArtistDashboard = () => {
                         <thead>
                             <tr className="border-b">
                                 <th className="text-left py-3">Artwork</th>
-                                <th className="text-left py-3">Buyer</th>
-                                <th className="text-left py-3">Date</th>
+                                <th className="text-left py-3 pl-2">Buyer</th>
+                                <th className="text-left py-3"></th>
+                                <th className="text-left py-3"></th>
+
                                 <th className="text-left py-3">Amount</th>
+                                <th className="text-left py-3"></th>
+
+                                <th className="text-left pl-2 py-3">Date</th>
+
                             </tr>
                         </thead>
 
                         <tbody>
-                            <tr className="border-b">
-                                <td className="py-3">Sunset Painting</td>
-                                <td>John Doe</td>
-                                <td>24 Jun 2026</td>
-                                <td>$120</td>
-                            </tr>
+                            {dashboard?.recentSales?.map((sale) => (
+                                <tr key={sale._id} className="border-b">
+                                    <td className="py-3 ">{sale.artwork}</td>
+                                    <td className="pl-2">{sale.buyer}  </td>
+                                    <td className="text-left pl-2 py-3"></td>
+                                    <td className="text-left py-3"></td>
 
-                            <tr>
-                                <td className="py-3">Digital Nature</td>
-                                <td>Sarah Smith</td>
-                                <td>22 Jun 2026</td>
-                                <td>$90</td>
-                            </tr>
+                                    <td>${sale.amount}  </td>
+                                    <td className="text-left py-3"></td>
+
+                                    <td>
+                                        {new Date(sale.date).toLocaleDateString()}
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
 
                     </table>
